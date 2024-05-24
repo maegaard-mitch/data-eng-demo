4a27d876980c06f978b73cb30ad867f7063ede08
Mitch Maegaard <maegaard.mitchell@gmail.com>
2024-05-24 11:26:24 -0600

    show pandera validation

--- src/__main__.py
+++ src/__main__.py
@@ -1,2 +1,17 @@
+import polars as pl
+import pandera.polars as pa
+
+class Schema(pa.DataFrameModel):
+    id: pl.Int64 = pa.Field(ge=0, le=3)
+    col2: pl.String = pa.Field(str_startswith="row")
+    col3: pl.String
+
+    class Config:
+        name = "BaseSchema"
+        strict = True
+        coerce = True
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    df = pl.read_csv("data/file.csv", separator="|").lazy()
+    validated_df = Schema.validate(df.collect(), lazy=True)
+    print(validated_df)
